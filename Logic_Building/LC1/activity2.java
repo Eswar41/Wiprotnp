25049727d066dc0d45ccb4284907d94b59973565
Eswar41 <eswargurram2005@gmail.com>
2026-07-17 20:26:35 +0530

    Updated Java programs

---
+++ Logic_Building/LC1/activity2.java
@@ -0,0 +1,28 @@
+package Logic_Building.LC1;
+
+import java.util.Scanner;
+
+public class activity2 {
+    public class activity2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int pass = 0;
+
+        for(int i = 1; i <= 3; i++) {
+
+            System.out.print("Enter marks of student " + i + ": ");
+            int marks = sc.nextInt();
+
+            if(marks >= 35)
+                pass++;
+        }
+
+        System.out.println("Students Passed = " + pass);
+
+        sc.close();
+    }
+}
+}
