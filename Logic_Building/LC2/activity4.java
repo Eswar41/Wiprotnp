96b801b52a1ad52d8226160bb7ac277da50262bb
Eswar41 <eswargurram2005@gmail.com>
2026-07-17 21:37:32 +0530

    Updated Java programs

---
+++ Logic_Building/LC2/activity4.java
@@ -0,0 +1,19 @@
+package Logic_Building.LC2;
+
+import java.util.Scanner;
+
+public class activity4 {
+     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Binary Number: ");
+        String binary = sc.next();
+
+        int decimal = Integer.parseInt(binary, 2);
+
+        System.out.println("Decimal = " + decimal);
+
+        sc.close();
+    }
+}
---
+++ Logic_Building/LC4/activity1.java
@@ -0,0 +1,26 @@
+package Logic_Building.LC4;
+
+import java.util.Scanner;
+
+public class activity1 {
+    public class Activity1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+
+        System.out.println("Reversed String: " + reverse);
+
+        sc.close();
+    }
+}
+}
